dfa6ceb079ebc380d0fd28fa0511892cdacdf4cc
Sai-sindhu-ch <chavasaisindhu1999@gmail.com>
2019-06-28 13:21:50 +0530

    commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,19 +1,69 @@
+/**
+ * 
+ */
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
 
+/**
+ * 
+ * @author sindhu
+ * UserController class
+ */
 @RestController
 public class UserController {
-//	@RequestMapping
-//	public String Check() {
-//		return "hello world";
-	//}
-	@RequestMapping("/User")
-	public User check() {
-		User user = new User("female","sindhu");
-		return user;
+	@Autowired
+	UserService userService;
+	@RequestMapping("/")
+	public String check() {
+		return "hello world";
+	}
+	
+	/**
+	 * 
+	 * @param name
+	 * @param gender
+	 * @return user by using name and gender
+	 */
+	@GetMapping("/User/{name}/{gender}")
+	public User retrieveByNameAndGender(@PathVariable("name") String name,@PathVariable("gender")String gender) {
+		return userService.retrieveByNameAndGender(name,gender);
+	}
+	/**
+	 * 
+	 * @param user
+	 * @return user
+	 */
+	@PostMapping("/User")
+	public User postMethod(@RequestBody User user) {
+		return userService.create(user);
+	}
+	/**
+	 * 
+	 * @param name
+	 * @param id
+	 */
+	@PutMapping("/User/{name}/{id}")
+	public void updateName(@PathVariable("name") String name,@PathVariable("id") int id) {
+		 userService.updateName(name,id);
+	}
+	/**
+	 * 
+	 * @param id
+	 * @return 
+	 */
+	@DeleteMapping("/User/{id}")
+	public User deleteById(@PathVariable int  id) {
+		return userService.deleteById(id);
 	}
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,22 +1,95 @@
 package com.example.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+/**
+ * 
+ * @author sindhu
+ * entity class User
+ */
+@Entity
 public class User {
-	private String gender;
-	private String name;
-	public String getGender() {
-		return gender;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public int id;
+	public String gender;
+	public String name;
+//	public String address;
+//	public int number;
+	public User(){
+		super();
 	}
-	public void setGender(String gender) {
-		this.gender = gender;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		//result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + id;
+		return result;
 	}
-	public String getName() {
-		return name;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		//if (address == null) {
+			//if (other.address != null)
+				//return false;
+		//} else if (!address.equals(other.address))
+			//return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public  User(String gender,String name) {
-		this.gender = gender;
-		this.name = name;
+		public String getGender() {
+			return gender;
+		}
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+//		public String getAddress() {
+//			return address;
+//		}
+//		public void setAddress(String address) {
+//			this.address = address;
+//		}
+		public int getId() {
+			return id;
+		}
+		public void setId(long id) {
+			this.id = (int) id;
+		}
+		public  User(String gender,String name,int id) {
+			this.gender = gender;
+			this.name = name;
+//			this.address = address;
+			this.id = id;
+		}
+	public String toString() {
+		return this.gender + " " + this.name + " "+ this.id; 
 	}
 }
